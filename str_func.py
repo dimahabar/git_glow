b737e0961de4e17f2f64367990cba62baca330df
dimahabar <93959889+dimahabar@users.noreply.github.com>
2023-06-11 18:35:09 +1000

    Revert "Hotfix"

--- str_func.py
+++ str_func.py
@@ -1,7 +1,3 @@
 def str_func(text):
     '''первая буква в верхнем регистре'''
     return str.swapcase(text)
-
-def upcase_litter(text):
-    '''все буквы в верхнем регистре'''
-    return stp.title(text)
