25880a701b42faf83140d88203b97fb6097dd128
Dmitriy Kovaluk <kdi.86@mail.ru>
2023-06-11 18:28:06 +1000

    add str_func

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def str_func(text):
+    '''первая буква в верхнем регистре'''
     return str.swapcase(text)
+
+def upcase_litter(text):
+    '''все буквы в верхнем регистре'''
+    return str.title(text)
